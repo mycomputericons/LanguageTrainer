62baeb1530de4e10b9da7dbb076ff2318adaa4fe
mycomputericons <mycomputericons@gmail.com>
2017-07-01 11:28:25 +0200

    Empty spring boot app

---
+++ src/main/java/com/LanguageTrainer/LanguageTrainer/LanguageTrainerApplication.java
@@ -0,0 +1,12 @@
+package com.LanguageTrainer.LanguageTrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageTrainerApplication.class, args);
+	}
+}
